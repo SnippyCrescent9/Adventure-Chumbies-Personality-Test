9374b9d63f51537f4af08f2ef2e188f7f56b6df2
Adam <adamsannes418@gmailcom>
2025-02-07 12:00:56 -0800

    Added mission statement in about page and added hook message on home page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 function About(){
     return(
         <div>
-            <h1>This is the About page!</h1>
+            <Header/>
             <Navbar/>
+            <h2>Our Mission</h2>
+            <p>
+            At Adventure Chumbies, we’re dedicated to making your travels more exciting, effortless, and fulfilling. 
+            We know your time, energy, and finances are valuable, and we believe they should be spent enjoying unforgettable adventures—not stressing over where to go next. 
+            That’s why we take the guesswork out of travel planning by helping you discover destinations that truly match your interests and travel style. 
+            With Adventure Chumbies, you can spend less time deciding and more time exploring! 
+            </p>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ function Home(){
         <div>
             <Header/>
             <Navbar/>
+            <p>Every great adventure deserves the perfect travel companion! At Adventure Chumbies, we’ve created a fun and interactive way to discover your ideal trip by matching you with your very own Adventure Chumbie.
+               Take our personality quiz, and you will be paired with a Chumbie—a unique animal that reflects your travel style, dream destinations, and sense of adventure. 
+               Whether you're a mountain climber, city explorer, or beach lover, your Chumbie reveals the destinations that will invigorate your travels—and might even inspire you to explore places you’ve never considered before! 
+               Take the test, meet your Chumbie, and let the adventure begin! 🌍🐾</p>
         </div>
     );
 }
